b01dc1989ba4411a4cc7621a8dedb229e30e959a
Maxim B <wedontask@gmail.com>
2022-06-19 16:08:28 +0300

    Add API to get user's permissions

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,11 +1,14 @@
 import logging
 
 from flask import abort, request
+from flask_jwt_extended.utils import get_jwt_identity
 from flask_jwt_extended.view_decorators import jwt_required
 from flask_restx import Resource
 from marshmallow import ValidationError
+from werkzeug.exceptions import BadRequest, HTTPException, NotFound
 
 from app.api.v1.dto import UserDto
+from app.models.permissions import PermissionsSchema
 from app.models.roles import RoleSchema
 from app.models.users import UserSchema
 from app.services.users import get_users_service
@@ -21,6 +24,7 @@ user_schema_resp = UserSchema(exclude=["password_hash"])
 users_schema = UserSchema(many=True)
 role_schema = RoleSchema()
 roles_schema = RoleSchema(many=True)
+permissions_schema = PermissionsSchema(many=True)
 
 api_service = get_users_service()
 
@@ -41,8 +45,8 @@ class UsersAPI(Resource):
 
     @jwt_required()
     @ns.doc(description="Удаление пользователя по id.")
-    @ns.response(204, 'User has been deleted.')
-    @ns.response(404, 'ID not found.')
+    @ns.response(204, "User has been deleted.")
+    @ns.response(404, "ID not found.")
     def delete(self, id: int):
         result = api_service.delete(id=id)
         if result:
@@ -89,8 +93,8 @@ class UsersRolesAPI(Resource):
     @jwt_required()
     @ns.expect(UserDto.user_roles_req)
     @ns.doc(description="Удаление ролей пользователя.")
-    @ns.response(204, 'Roles has been deleted.')
-    @ns.response(404, 'ID not found.')
+    @ns.response(204, "Roles has been deleted.")
+    @ns.response(404, "ID not found.")
     def delete(self, id: int):
         result = api_service.delete_roles(id=id, roles_id=dict(request.json)["ids"])
         if result:
@@ -100,10 +104,23 @@ class UsersRolesAPI(Resource):
     @jwt_required()
     @ns.expect(UserDto.user_roles_req)
     @ns.doc(description="Изменение ролей пользователя.")
-    @ns.response(201, 'Roles has been updated.')
-    @ns.response(404, 'ID not found.')
+    @ns.response(201, "Roles has been updated.")
+    @ns.response(404, "ID not found.")
     def post(self, id: int):
         result = api_service.add_roles(id=id, roles_id=dict(request.json)["ids"])
         if result:
             return ok20x(http_code=201)
         return abort(404)
+
+
+# @ns.expect(UserDto.user_roles_req)
+@ns.marshal_list_with(UserDto.user_permissions_res)
+@ns.route("/permissions", doc={"description": "Получение прав пользователя"})
+class UserPermissionsAPI(Resource):
+    @jwt_required()
+    @ns.response(200, "Successfully get user permissions.")
+    @ns.response(404, "User not found")
+    def post(self):
+        identity = get_jwt_identity()
+        result = api_service.get_user_permissons(identity)
+        return permissions_schema.dump(result)
